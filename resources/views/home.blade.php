0062ecbd66a3d9a3ae1a29380d4c99125107a8d9
jddev81 <47901060+jddev81@users.noreply.github.com>
2019-03-07 12:43:15 -0500

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+
+@section('content')
+
+	<div class="home-page-wrapper">
+		<div class="container-flui">
+			<div class="row no-gutters">
+				<div class="col-sm-6">
+					<div class="hero">
+						<img class="img-fluid" src="/img/logo.png" alt="Milton Eats">
+					</div>
+				</div>
+				<div class="col-sm-6">
+					this is other stuff
+				</div>
+			</div>
+		</div>
+	</div>
+
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+
+		<meta charset="utf-8">
+
+		<link rel="shortcut icon" href="img/favicon.ico">
+
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<!--STYLESHEETS-->
+		<link rel="stylesheet" href="/css/app.css">
+		<!--FONTS-->
+
+		<!--JS-->
+
+		<title></title>
+
+		<!--[if IE]>
+			<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+
+	</head>
+
+	<body>
+
+		@yield('content')
+
+	</body>
+</html>
