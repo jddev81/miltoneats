a6fe2b228bffc06328765dd55728d7ba5ddb8440
jddev81 <47901060+jddev81@users.noreply.github.com>
2019-03-08 16:02:54 -0500

    Add Form On Home Page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,7 +12,28 @@
 					</div>
 				</div>
 				<div class="col-sm-6">
-					this is other stuff
+					<div class="hero-content">
+						<nav class="main-nav">
+
+						</nav>
+						<div class="selection">
+							
+							<h1>Find the food you love in Milton, WV.</h1>
+						
+							<form action="#" method="POST">
+								<select name="food_type">
+									<option vlaue="#">Enter Food Type</option>
+									<option value="#">Dine In</option>
+									<option vlaue="#">Carry Out</option>
+									<option value="pizza">Pizza</option>
+									<option value="burgers">Burgers</option>
+									<option value="other">Other</option>
+								</select>
+								<button type="submit" name="submit" class="submit"><i class="fas fa-caret-right"></i></button>
+							</form>
+							
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
 		<link rel="stylesheet" href="/css/app.css">
 		<!--FONTS-->
 		<link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700" rel="stylesheet">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 		<!--JS-->
 
 		<title></title>
