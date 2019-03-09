b51630da0e4f36253f23edb167788af335ff4609
jddev81 <47901060+jddev81@users.noreply.github.com>
2019-03-09 07:42:45 -0500

    Add nav and icon on form

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,13 +14,17 @@
 				<div class="col-sm-6">
 					<div class="hero-content">
 						<nav class="main-nav">
-
+							<ul>
+								<li><a href="#">Create Account</a></li>
+								<li><a href="#">Sign In</a></li>
+							</ul>
 						</nav>
 						<div class="selection">
 							
 							<h1>Find the food you love in Milton, WV.</h1>
 						
 							<form action="#" method="POST">
+								<span class="icon"><i class="fas fa-hand-pointer"></i></span>
 								<select name="food_type">
 									<option vlaue="#">Enter Food Type</option>
 									<option value="#">Dine In</option>
@@ -50,13 +54,13 @@
 					</div>
 					<div class="col-sm-4">
 						<img class="icon-img" src="/img/icons/takeOutPack/011-favorite.png" alt="Favorite Restaurants">
-						<h2>Favorite Restaurants</h2>
+						<h2>Save Your Favorite Restaurants</h2>
 						<p>Save your favorite restaurants for quick access on your next visit.</p>
 					</div>
 					<div class="col-sm-4">
 						<img class="icon-img" src="/img/icons/takeOutPack/037-reward.png" alt="Rewarded With Free Food">
-						<h2>Free Food</h2>
-						<p>Create an account to be entered into our monthly drawing for a free gift certificate.</p>
+						<h2>Get Free Food</h2>
+						<p>Create an account and you will be entered automatically every month in our drawing for a free gift certificate.</p>
 					</div>
 				</div>
 			</div>
